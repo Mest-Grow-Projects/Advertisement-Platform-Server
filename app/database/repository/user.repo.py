821ae010cfffc1e8c627214d3600b1335b19bf05
george-asiedu <asiedug41@gmail.com>
2025-09-09 08:17:14 +0000

    email converted to lowercase letters and returns false instead of true

--- app/database/repository/user.repo.py
+++ app/database/repository/user.repo.py
@@ -4,10 +4,10 @@ from app.database.models.user import User
 
 
 async def check_existing_user(email: str) -> bool:
-    user = await User.find_one(User.email == email)
+    user = await User.find_one(User.email == email.lower())
     if user:
         raise HTTPException(
             status_code=status.HTTP_400_BAD_REQUEST,
             detail=f"User {email} already exists, please login"
         )
-    return True
+    return False
