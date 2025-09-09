a1e5d1c929fbb736d791bdc53ec15a666272bee5
george-asiedu <asiedug41@gmail.com>
2025-09-09 07:56:15 +0000

    application lifespan event implemented to initialize db connection on application startup and close connection on app shutdown

--- app/main.py
+++ app/main.py
@@ -1,9 +1,24 @@
 from fastapi import FastAPI
+from contextlib import asynccontextmanager
+from app.database.db import init_db
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    await init_db()
+    print("DB Connection initialized")
+
+    try:
+        yield
+    finally:
+        print("DB Connection closed")
+
 
 app = FastAPI(
     title="Advertisement Management Platform API Documentation",
     description="API documentation for the Advertisement Management Platform.",
     version="1.0.0",
+    lifespan=lifespan
 )
 
 @app.get(
