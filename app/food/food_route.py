9c853f0acb1c9f241884a8bac44f86addafbd72c
Py-Me23 <oforiafia1@gmail.com>
2025-09-09 16:53:30 +0000

    added the get,post and delete endpoints

--- app/food/food_route.py
+++ app/food/food_route.py
@@ -7,12 +7,36 @@ router = APIRouter(tags=["Food"])
 
 
 @router.post("/food_ad")
-def post_food_ads(data: FoodSchema):
+async def post_food_ads(data: FoodSchema):
     food = Food(
         name=data.name,
         description=data.description,
         category=data.category,
         price=data.price,
     )
-    food.insert()
+    await food.insert()
     return {"message": "You have successfully added an ad"}
+
+
+@router.get("/all")
+async def get_all_food_ads(limit=10, skip=0):
+    foods = await Food.find(
+        # filter={
+        #     "$or": [
+        #         {"name": {"$regex": data.name, "$options": "i"}},
+        #         {"description": {"$regex": data.description, "$options": "i"}},
+        #     ]
+        # },
+        # limit=int(limit),
+        # skip=int(skip),
+    ).to_list()
+    return {"data": foods}
+
+
+@router.delete("/{food_id}")
+async def delete_food_ad(food_id):
+
+    deleted_ad = await Food.delete_one(food_id)
+    if not deleted_ad:
+        raise HTTPException(status.HTTP_404_NOT_FOUND, "No add found to delete!")
+    return {"message": "Ad successfully deleted!"}
